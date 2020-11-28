27b1af820f82f1d89e27c780c3f15460ae107521
Marcelo <mabc@live.cl>
2020-11-28 20:02:53 -0300

    En Backend se creo el modelo de base de datos e implementó jwt, Se creo Frontend

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -15,8 +15,7 @@ class User extends Authenticatable implements JWTSubject
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
-     */
+     * @var array     */
     protected $fillable = [
         'name',
         'email',
