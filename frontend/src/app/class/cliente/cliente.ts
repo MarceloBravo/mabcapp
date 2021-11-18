12abfe9bd6c354f9f664b8de2b861259840b74d1
Marcelo <mabc@live.cl>
2021-11-18 20:26:57 -0300

    Se inició la rama del mantenedor de clientes

---
+++ frontend/src/app/class/cliente/cliente.ts
@@ -0,0 +1,24 @@
+export class Cliente {
+    id: number | null = null;
+    rut: string = '';
+    nombres: string = '';
+    apellido1: string = '';
+    apellido2: string = '';
+    cod_region: string = '';
+    cod_provincia: string = '';
+    cod_comuna: string = '';
+    ciudad: string = '';
+    direccion: string = '';
+    password: string = '';
+    confirm_password: string = '';
+    email: string = '';
+    fono: string = '';
+    foto: object = {};
+    nombre_foto: string = '';
+    casa_num: string = '';
+    block_num: string = '';
+    referencia: string = '';
+    created_at: string = '';
+    updated_at: string = '';
+    deleted_at: string = '';
+}
