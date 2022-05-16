2e4d53343f7e875a78c22d393057ffa58583a34a
Marcelo <mabc@live.cl>
2022-05-15 23:47:17 -0400

    Se modificó el efecto al pasar el mouse sobre un producto y se corrigieron errores menores varios

--- frontend/src/app/services/constantes/constantes.service.ts
+++ frontend/src/app/services/constantes/constantes.service.ts
@@ -8,8 +8,8 @@ import { TokenService } from '../token/token.service';
 export class ConstantesService {
   public endPoint = "http://localhost:8000/api/"  //Ruta local de las peticiones al backend
   public storageImages = "http://localhost:8000/storage/" //Ruta de almnacenamiento de las imágenes en el backend
-  //public endPoint = "http://192.168.xx.xxx:3000/api/" //Ruta pública de las peticiones al backend
-  //public storageImages = "http://192.168.xx.xxx:3000/storage/" //Ruta de almnacenamiento de las imágenes en el backend
+  //public endPoint = "http://192.168.43.118:3000/api/" //Ruta pública de las peticiones al backend
+  //public storageImages = "http://192.168.43.118:3000/storage/" //Ruta de almnacenamiento de las imágenes en el backend
   public srcDefault: string = '/assets/images/users/user.png'
   public noImage: string = '/assets/front-office/img/bg-img/no_image.png'
   public apiDigitalGob: string = 'https://apis.digital.gob.cl/dpa/'
